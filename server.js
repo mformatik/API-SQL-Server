5702b4a17f65995a1b6a288b072e824a2630e79b
Mazouzi Aymene <39919087+El-Mazouzi@users.noreply.github.com>
2021-11-30 08:50:47 +0100

    .

--- server.js
+++ server.js
@@ -74,61 +74,7 @@ app.post(
   }
 );
 
-app.post(
-  "/getDatabases1",
-  [
-    check("ip", "ip is required").not().isEmpty(),
-    check("username", "username is required").not().isEmpty(),
-    check("password", "password is required").not().isEmpty(),
-  ],
-  async (req, res) => {
-    try {
-      let errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
 
-      const query = "SELECT name FROM sys.databases WHERE name LIKE 'Hisba_%';";
-      const ip = req.body.ip;
-      const username = req.body.username;
-      const password = req.body.password;
-      const database = "master";
-      const sql = require("mssql");
-      var config = {
-        user: username,
-        password: password,
-        server: ip,
-        database: database,
-        options: {
-          encrypt: false,
-          enableArithAbort: true,
-          cryptoCredentialsDetails: {
-            minVersion: "TLSv1",
-          },
-        },
-        synchronize: true,
-      };
-
-      const pool = new sql.ConnectionPool(config);
-      try {
-        await pool.connect();
-        const request = pool.request();
-        let result = await request.query(`${query}`);
-
-        return res.json(result.recordset);
-      } catch (error) {
-        console.error(error);
-        throw new Error(error);
-      } finally {
-        pool.close(); // closing connection after request is finished
-      }
-    } catch (error) {
-      console.error("error");
-      console.error(error.message);
-      return res.status(500).json({ msg: "Error" });
-    }
-  }
-);
 app.post(
   "/getQuery",
   [
@@ -196,59 +142,5 @@ app.post(
   }
 );
 
-app.post(
-  "/getQuery1",
-  [
-    check("ip", "ip is required").not().isEmpty(),
-    check("username", "username is required").not().isEmpty(),
-    check("password", "password is required").not().isEmpty(),
-    check("database", "database is required").not().isEmpty(),
-  ],
-  async (req, res) => {
-    try {
-      let errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      const query = req.body.query;
-      const ip = req.body.ip;
-      const username = req.body.username;
-      const password = req.body.password;
-      const database = req.body.database;
-      var config = {
-        user: username,
-        password: password,
-        server: ip,
-        database: database,
-        options: {
-          encrypt: false,
-          enableArithAbort: true,
-          cryptoCredentialsDetails: {
-            minVersion: "TLSv1",
-          },
-        },
-        synchronize: true,
-      };
-
-      const pool = new sql.ConnectionPool(config);
-      try {
-        await pool.connect();
-        const request = pool.request();
-        let result = await request.query(`${query}`);
-
-        return res.json(result.recordset);
-      } catch (error) {
-        console.error(error);
-        throw new Error(error);
-      } finally {
-        pool.close(); // closing connection after request is finished
-      }
-    } catch (error) {
-      console.error("error");
-      console.error(error.message);
-      return res.status(500).json({ msg: "Error" });
-    }
-  }
-);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
